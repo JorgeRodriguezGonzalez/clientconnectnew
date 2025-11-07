33ed8c0c40ac12424bd02fba6d0db150d98fd641
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-07 16:00:34 +1100

    Fix HowWeWork import - use named export

--- src/components/ui/animated-gallery.tsx
+++ src/components/ui/animated-gallery.tsx
@@ -1,5 +1,3 @@
-"use client" 
-
 import * as React from "react"
 
 import {
@@ -9,7 +7,7 @@ import {
   motion,
   useScroll,
   useTransform,
-} from "motion/react"
+} from "framer-motion"
 
 import { cn } from "@/lib/utils"
 
