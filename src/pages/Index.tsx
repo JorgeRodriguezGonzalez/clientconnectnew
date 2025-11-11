aa577f51ee347eb7d405e1e77ac078a59ca95df7
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-11 12:01:37 +1100

    Fix HowWeWork import - use named export

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { HeroNew } from "@/components/home/HeroNew";
+import { GoogleGeminiEffectDemo } from "@/components/home/GoogleGeminiEffectDemo";
 import { AnimatedGallerySection } from "@/components/animated-gallery-section";
 import ServicesSection from "@/components/home/ServicesSection";
 import WhyUsSection from "@/components/home/WhyUsSection";
@@ -16,6 +17,7 @@ const Index = () => {
       <Header />
       <main className="flex-1">
         <HeroNew />
+        <GoogleGeminiEffectDemo />
         <AnimatedGallerySection />
         <ServicesSection />
         <WhyUsSection />
