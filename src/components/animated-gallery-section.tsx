72f830240798ed7c88ce11744dfb33f47059e3b7
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-11 11:45:37 +1100

    Fix HowWeWork import - use named export

--- src/components/animated-gallery-section.tsx
+++ src/components/animated-gallery-section.tsx
@@ -14,14 +14,14 @@ import {
   const IMAGES_2 = [
     "/images/5.png",
     "/images/6.png",
-    "/lovable-uploads/bd7a31ea-72e3-4ba4-9f93-00ba3f6bc43e.png",
-    "/lovable-uploads/8f2bb631-9f79-45d4-bfd5-65d57179aa91.png",
+    "/images/7.jpg",
+    "/images/8.jpg",
   ]
   const IMAGES_3 = [
-    "/lovable-uploads/9f96d878-2f42-4f3f-9c8c-54b90b54a4e7.png",
-    "/lovable-uploads/e3118069-484b-4605-a1e1-2da88f4e4230.png",
-    "/lovable-uploads/ae4e2c3b-76b3-4743-a685-daa06511529e.png",
-    "/lovable-uploads/98d69262-4c1c-46b7-b307-b0569c06b499.png",
+    "/images/9.jpg",
+    "/images/10.jpg",
+    "/images/11.jpg",
+    "/images/12.jpg",
   ]
   
   export const AnimatedGallerySection = () => {
