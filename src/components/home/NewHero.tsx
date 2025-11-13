168b92892283a3aa7bfda138a93a2cdd6907e508
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-13 15:01:13 +1100

    Add glassmorphism scroll effect to hero section

--- src/components/home/NewHero.tsx
+++ src/components/home/NewHero.tsx
@@ -231,7 +231,7 @@ export function NewHero() {
             {/* Sparkles Effect */}
             <div className="relative -mt-32 h-96 w-full overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)]">
               <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#06B6D4,transparent_70%)] before:opacity-40" />
-              <div className="absolute -left-1/2 top-[45%] aspect-[1/0.7] z-10 w-[200%] rounded-[100%] border-t border-l border-r border-white/20" style={{ backgroundColor: 'rgb(10, 15, 35)' }} />
+              <div className="absolute -left-1/2 top-[65%] aspect-[1/0.7] z-10 w-[200%] rounded-[100%] border-t border-white/20" style={{ backgroundColor: 'rgb(10, 15, 35)' }} />
               <Sparkles
                 density={1200}
                 className="absolute inset-x-0 bottom-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
