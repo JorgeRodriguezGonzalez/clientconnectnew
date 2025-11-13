26970aa5281022a343d44752e10bf462c5867ea0
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-13 15:32:52 +1100

    Add glassmorphism scroll effect to hero section

--- src/components/home/NewHero.tsx
+++ src/components/home/NewHero.tsx
@@ -67,7 +67,6 @@ export function NewHero() {
       style={{ backgroundColor }}
       className="h-[200vh] w-full dark:border dark:border-white/[0.1] rounded-md relative overflow-clip"
     >
-        <div className="sticky top-[-40px] h-screen flex justify-center pt-32"></div>
       <div className="sticky top-0 h-screen flex justify-center pt-32">
         <div className="z-10 flex flex-col items-center justify-center gap-4 max-w-[976px] px-5 relative">
 
