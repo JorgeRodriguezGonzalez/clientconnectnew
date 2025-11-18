98bb929c9bad1390ac88ca13596d4632089f8e6f
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-18 15:33:15 +1100

    Remove Sparkles section and rename NewHero to FinalHero

--- src/components/home/MarketingCard.tsx
+++ src/components/home/MarketingCard.tsx
@@ -141,7 +141,7 @@ const Container = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={`rounded-full flex items-center justify-center bg-[rgba(248,248,248,0.01)] shadow-[0px_0px_8px_0px_rgba(248,248,248,0.25)_inset,0px_32px_24px_-16px_rgba(0,0,0,0.40)] ${className}`}
+    className={`rounded-full flex items-center justify-center bg-[rgba(248,248,248,0.01)] shadow-[0px_-2px_4px_0px_rgba(0,0,0,0.06)_inset,0px_8px_12px_-8px_rgba(0,0,0,0.15)] ${className}`}
     {...props}
   />
 ));
