75967c2c08e9f2f394accf17fda0fec5cbb2a726
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-18 12:32:56 +1100

    Remove Sparkles section and rename NewHero to FinalHero

--- src/components/home/BrandsShowcaseCard.tsx
+++ src/components/home/BrandsShowcaseCard.tsx
@@ -120,7 +120,7 @@ export const BrandsShowcaseCard = ({
         </div>
       </div>
       
-      <div className="flex flex-col items-start justify-center gap-2 w-[673.2px] h-[70px] px-4 pt-5 pb-4">
+      <div className="flex flex-col items-start justify-center gap-2 w-[673.2px] h-[100px] px-4 pt-5 pb-4">
         <h2 className="font-medium text-white text-[18px] leading-[17px] tracking-[-0.6px] m-0" style={{
           fontFamily: 'Inter, "Inter Placeholder", sans-serif',
           fontStyle: 'normal'
