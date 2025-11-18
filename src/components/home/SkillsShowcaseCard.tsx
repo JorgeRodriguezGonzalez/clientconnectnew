c64dffd09b6d39d1c09c707ac976878d478df5e9
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-18 13:04:39 +1100

    Remove Sparkles section and rename NewHero to FinalHero

--- src/components/home/SkillsShowcaseCard.tsx
+++ src/components/home/SkillsShowcaseCard.tsx
@@ -9,13 +9,13 @@ type SkillsShowcaseCardProps = {
 
 const skills = [
   { text: 'Website Design', position: { top: '156px', right: '24px' } },
-  { text: 'SEO', position: { top: '88px', right: '20px' }, transform: 'translateY(-12px)' },
-  { text: 'Google Ads', position: { top: '58px', left: '29px' } },
+  { text: 'SEO', position: { top: '18px', right: '20px' }, transform: 'translateY(-12px)' },
+  { text: 'Google Ads', position: { top: '18px', left: '29px' } },
   { text: 'Social Media Management', position: { top: '45px', right: '56px' } },
   { text: 'CRM', position: { top: '8px', right: '53px' } },
-  { text: 'Facebook Ads', position: { bottom: '31px', left: '80px' } },
+  { text: 'Facebook Ads', position: { bottom: '61px', left: '80px' } },
   { text: 'Videography', position: { bottom: '14px', left: '50px' } },
-  { text: 'Photography', position: { bottom: '55px', left: '9px' } }
+  { text: 'Photography', position: { bottom: '85px', left: '9px' } }
 ] as any[];
 
 export const SkillsShowcaseCard = ({
