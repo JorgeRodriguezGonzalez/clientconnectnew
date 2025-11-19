12ad32229d3144419592bf3e3913a1a8899c2ceb
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-19 14:36:01 +1100

    tu mensaje aquí

--- src/components/home/FinalHero.tsx
+++ src/components/home/FinalHero.tsx
@@ -101,7 +101,7 @@ export function FinalHero() {
                 animate={{ opacity: 1, width: "30rem" }}
                 transition={{ delay: 0.8, duration: 1.0, ease: "easeInOut" }}
                 style={{ backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))` }}
-                className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-[#FFF8E1] via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+                className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
               >
                 <motion.div
                   className="absolute w-[100%] left-0 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]"
@@ -118,7 +118,7 @@ export function FinalHero() {
                 animate={{ opacity: 1, width: "30rem" }}
                 transition={{ delay: 0.8, duration: 1.0, ease: "easeInOut" }}
                 style={{ backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))` }}
-                className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-[#FFF8E1] text-white [--conic-position:from_290deg_at_center_top]"
+                className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
               >
                 <motion.div
                   className="absolute w-40 h-[100%] right-0 bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]"
@@ -144,21 +144,21 @@ export function FinalHero() {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 0.5 }}
                 transition={{ delay: 0.8, duration: 1.0, ease: "easeInOut" }}
-                className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-[#FFF8E1] blur-3xl"
+                className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 blur-3xl"
               />
 
               <motion.div
                 initial={{ opacity: 0, width: "8rem" }}
                 animate={{ opacity: 1, width: "16rem" }}
                 transition={{ delay: 0.8, duration: 1.0, ease: "easeInOut" }}
-                className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-[#FFF8E1] blur-2xl"
+                className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
               ></motion.div>
 
               <motion.div
                 initial={{ opacity: 0, width: "15rem" }}
                 animate={{ opacity: 1, width: "30rem" }}
                 transition={{ delay: 0.8, duration: 1.0, ease: "easeInOut" }}
-                className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-[#FFF8E1]"
+                className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400"
               ></motion.div>
 
               <motion.div
