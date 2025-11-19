10e44884631af1d0043dc0ba008c3c270d4320f4
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2025-11-19 16:06:40 +1100

    tu mensaje aquí

--- src/components/home/UseCasesShowcase.tsx
+++ src/components/home/UseCasesShowcase.tsx
@@ -41,7 +41,7 @@ export const UseCasesShowcase = (props: UseCasesShowcaseProps) => {
   };
 
   return (
-    <section ref={ref} className="relative bg-white -mt-64">
+    <section ref={ref} className="relative bg-white -mt-24">
       {/* ARCO EXACTAMENTE COMO ANTES, PERO SIN SOMBRA */}
       <div className="absolute inset-x-0 top-0 h-80 pointer-events-none">
         <div
