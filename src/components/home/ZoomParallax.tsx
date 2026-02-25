21913eb3a477d123e9adc28d506017a83d5c2114
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2026-02-25 13:42:41 +1100

    fix: replace premier video with new recording

--- src/components/home/ZoomParallax.tsx
+++ src/components/home/ZoomParallax.tsx
@@ -110,7 +110,7 @@ export default function ZoomParallax() {
     const videos = [
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771972854/0225_qidba6.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771974290/prolex_ewdmo7.mov" },
-        { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771980528/premier_n7wxah.mov" },
+        { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771987258/0225_spcg8h.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771978168/nanotise_s5oatf.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771978181/driveways_sdxoqa.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771978172/landscaping_lierf1.mov" },
