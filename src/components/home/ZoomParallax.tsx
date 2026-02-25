e18defc0828047cbc37bdbfe3242ad6dabd6709d
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2026-02-25 14:43:14 +1100

    revert: driveways video back to previous

--- src/components/home/ZoomParallax.tsx
+++ src/components/home/ZoomParallax.tsx
@@ -112,7 +112,7 @@ export default function ZoomParallax() {
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771989643/0225_1_jymeqw.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771987258/0225_spcg8h.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771978168/nanotise_s5oatf.mov" },
-        { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771990704/lcdriveways_khvzdq.mov" },
+        { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771978181/driveways_sdxoqa.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771978172/landscaping_lierf1.mov" },
         { src: "https://res.cloudinary.com/dsdnvhpmr/video/upload/v1771989643/0225_1_jymeqw.mov" },
     ];
