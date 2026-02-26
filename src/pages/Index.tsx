aad4f252ac85e34047358ac1672478eb675ea88f
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2026-02-27 10:28:46 +1100

    remove: BentoGrid block after ComparisonSection

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,9 +47,6 @@ const Index = () => {
         <WhatWeDoSection2 />
 
         <ComparisonSection />
-        <div className="w-full flex justify-center pb-16 relative z-[999999]">
-          <BentoGrid />
-        </div>
 
         <CaseStudiesSection />
 
