7cf0ce7803f14224c4b58ed0051e81adee9dc931
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2026-03-04 14:25:53 +1100

    remove video production, restore tab gap

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -290,7 +290,7 @@ const Services = () => {
 
         {/* Tabs */}
         <div className="relative mb-12">
-          <div ref={tabsContainerRef} className="flex overflow-x-auto gap-1 pb-4 -mx-4 px-4 md:mx-0 md:px-0 md:justify-center hide-scroll">
+          <div ref={tabsContainerRef} className="flex overflow-x-auto gap-2 pb-4 -mx-4 px-4 md:mx-0 md:px-0 md:justify-center hide-scroll">
             {SERVICES.map(service => {
               const isActive = activeTab === service.id;
               const isHovered = hoveredTab === service.id;
