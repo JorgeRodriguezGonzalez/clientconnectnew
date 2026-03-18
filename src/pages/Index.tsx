14a0fd8ae81d3a22a7f002e9829b61efe62429b9
JorgeRodriguezGonzalez <120454556+JorgeRodriguezGonzalez@users.noreply.github.com>
2026-03-19 10:28:53 +1100

    Apply section-title class to all 9 section components

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,13 @@
-import { HowWeWork } from "@/components/home/HowWeWork";
-import { WhySubscribeBento } from "@/components/home/WhySubscribeBento";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import CTASection from "@/components/home/CTASection";
-import { UseCasesShowcase } from "@/components/home/UseCasesShowcase";
 import ComparisonSection from "@/components/home/ComparisonSection";
-import Pricing from "@/components/home/Pricing";
-import BoxCards from "@/components/home/BoxCards";
 import FounderSection from "@/components/home/FounderSection";
-import OurSteps from "@/components/home/OurSteps";
-import ScrollableWorkflow from "@/components/home/ScrollableWorkflow";
-import { BlueprintVisualization } from "@/components/home/BlueprintVisualization";
-import { InteractiveCardStack } from "@/components/home/InteractiveCardStack";
-import CloudHero from "@/components/home/section-layers";
 import Services from "@/components/home/Services";
 import { SuperHero } from "@/components/home/SuperHero";
 import { FloatingBanner } from "@/components/home/floatingbanner";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import FAQSection from "@/components/home/faqs";
-
-import HowWeDoIt from "@/components/home/HowWeDoIt";
 import InteractivePath from "@/components/home/InteractivePath";
 import { WhatWeDoSection2 } from "@/components/home/WhatWeDoSection2";
 import ZoomParallax from "@/components/home/ZoomParallax";
